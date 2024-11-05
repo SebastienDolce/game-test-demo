7836495753226b23422b41dca1a7f0ec808100dc
Sebastien Dolce <sebastiendolce@gmail.com>
2024-11-05 10:24:42 -0500

    fixing hrefs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3748756324"
+    serviceWorkerVersion: "1295782479"
   }
 });
