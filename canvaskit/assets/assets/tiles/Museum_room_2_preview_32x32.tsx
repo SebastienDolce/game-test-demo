0300231a56e3c84fa2d17a556c36969a43b0bab6
Sebastien Dolce <sebastiendolce@gmail.com>
2024-11-18 12:10:49 -0500

    rm everything

--- canvaskit/assets/assets/tiles/Museum_room_2_preview_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Museum_room_2_preview_32x32" tilewidth="32" tileheight="32" tilecount="528" columns="16">
- <image source="Museum_room_2_preview_32x32.png" width="512" height="1056"/>
-</tileset>
