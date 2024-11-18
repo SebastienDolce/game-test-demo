c6df9815c8824eb150746b4a5bbb088fc85d0ff9
Sebastien Dolce <sebastiendolce@gmail.com>
2024-11-17 22:53:46 -0500

    forgot to turn off debug mode

--- assets/flutter_service_worker.js
+++ assets/flutter_service_worker.js
@@ -520,14 +520,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "621e9b04862b995e4c3349afef438dc8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "470ccc0e910e9f9034a569e0f8c14ac3",
+"flutter_bootstrap.js": "66e5950d44c8f95ee6168018721790b2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e85332cb5983e7cb86b84e52bffb20ff",
 "/": "e85332cb5983e7cb86b84e52bffb20ff",
-"main.dart.js": "4945b1de4dad75cc4fed6a5485219e98",
+"main.dart.js": "7bc446cab61a9b0a3d53c21ca945655a",
 "manifest.json": "1976c78e87fb28ba203f5bd5e64afb7e",
 "version.json": "0b0e8f66ee7d6472348f695eec23a304"};
 // The application shell files that are downloaded before a service worker can
