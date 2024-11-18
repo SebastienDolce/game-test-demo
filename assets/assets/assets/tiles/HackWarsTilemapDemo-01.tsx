423a35fa36caedbad7ec1777190e0bcf3226fc22
Sebastien Dolce <sebastiendolce@gmail.com>
2024-11-17 23:04:03 -0500

    delete everything

--- assets/assets/assets/tiles/HackWarsTilemapDemo-01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="HackWarsTilemapDemo-01" tilewidth="32" tileheight="32" tilecount="8500" columns="100">
- <image source="HackWarsTilemapDemo-01.png" width="3200" height="2720"/>
-</tileset>
--- assets/assets/tiles/Museum_room_4_layer_1_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Museum_room_4_layer_1_32x32" tilewidth="32" tileheight="32" tilecount="160" columns="16">
- <image source="Museum_room_4_layer_1_32x32.png" width="512" height="320"/>
-</tileset>
