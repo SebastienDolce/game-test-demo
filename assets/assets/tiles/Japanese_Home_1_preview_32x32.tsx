f7f3144a20112ca9cb675ff9bc527e51920071e9
Sebastien Dolce <sebastiendolce@gmail.com>
2024-12-07 13:52:18 -0500

    delete everything

--- assets/assets/tiles/Japanese_Home_1_preview_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Japanese_Home_1_preview_32x32" tilewidth="32" tileheight="32" tilecount="247" columns="19">
- <image source="Japanese_Home_1_preview_32x32.png" width="608" height="428"/>
-</tileset>
--- assets/assets/tiles/animated_control_room_facebook_scrolling_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="animated_control_room_facebook_scrolling_32x32" tilewidth="32" tileheight="32" tilecount="24" columns="12">
- <image source="animated_control_room_facebook_scrolling_32x32.png" width="384" height="64"/>
-</tileset>
